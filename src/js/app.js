f7b1acece1b3b2c804147325aad6381e6664afeb
mohamadadithya <mohamadadithya@gmail.com>
2021-11-14 00:34:36 +0700

    fix the style and add data exception

--- src/js/app.js
+++ src/js/app.js
@@ -17,59 +17,61 @@ const getUser = async () => {
 }
 
 const showUser = (data) => {
-    let userCardHTML = `<div class="head d-flex center">
-    <img src="${data.avatar_url}" alt="${data.name}" class="photo">
-    <div class="d-flex between w-100 sub">
-        <div>
-            <h1 class="name fw-bold">${data.name}</h1>
-            <a href="${data.html_url}" class="username" target="_blank">@${data.login}</a>
-        </div>
-        <p class="joined">Joined ${new Date(data.created_at).toLocaleDateString('en-US')}</p>
-    </div>
-</div>
-<div class="details">
-    <p class="bio">${(data.bio) ? data.bio : '-'}</p>
-    <ul class="card d-flex between center">
-        <li>
-            <h6 class="mb-5">Repos</h6>
-            <span class="fw-bold">${data.public_repos}</span>
-        </li>
-        <li>
-            <h6 class="mb-5">Followers</h6>
-            <span class="fw-bold">${data.followers}</span>
-        </li>
-        <li>
-            <h6 class="mb-5">Following</h6>
-            <span class="fw-bold">${data.following}</span>
-        </li>
-    </ul>
-    <div class="links d-flex between">
-        <ul>
-            <li>
-                <i class="fas fa-fw fa-map-marked-alt"></i>
-                <span>${(data.location) ? data.location : '-'}</span>
-            </li>
-            <li>
-                <i class="fas fa-fw fa-link"></i>
-                <span>${(data.blog) ? data.blog : '-'}</span>
-            </li>
-        </ul>
-        <ul>
-            <li>
-                <i class="fab fa-fw fa-twitter"></i>
-                <span>${(data.twitter_username) ? data.twitter_username : '-'}</span>
-            </li>
-            <li>
-                <i class="fas fa-fw fa-building"></i>
-                <span>${(data.company) ? data.company : '-'}</span>
-            </li>
-        </ul>
-    </div>
-</div>`
-    if(data === null) {
-        console.log('Ok')
+    if (data.message === 'Not Found') {
+        loader.innerHTML = 'User not found.'
+        userCardContainer.innerHTML = loader.innerHTML
     } else {
-        userCardContainer.innerHTML = userCardHTML
+        let userCardHTML = `<div class="head d-flex center">
+        <img src="${data.avatar_url}" alt="${data.name}" class="photo">
+        <div class="d-flex between w-100 sub">
+            <div>
+                <h1 class="name fw-bold">${(data.name) ? data.name : '-'}</h1>
+                <a href="${data.html_url}" class="username" target="_blank">@${data.login}</a>
+            </div>
+            <p class="joined">Joined ${new Date(data.created_at).toLocaleDateString('en-US')}</p>
+        </div>
+    </div>
+    <div class="details">
+        <p class="bio">${(data.bio) ? data.bio : '-'}</p>
+        <ul class="card d-flex between center">
+            <li>
+                <h6 class="mb-5">Repos</h6>
+                <span class="fw-bold">${data.public_repos}</span>
+            </li>
+            <li>
+                <h6 class="mb-5">Followers</h6>
+                <span class="fw-bold">${data.followers}</span>
+            </li>
+            <li>
+                <h6 class="mb-5">Following</h6>
+                <span class="fw-bold">${data.following}</span>
+            </li>
+        </ul>
+        <div class="links d-flex between">
+            <ul>
+                <li>
+                    <i class="fas fa-fw fa-map-marked-alt"></i>
+                    <span>${(data.location) ? data.location : '-'}</span>
+                </li>
+                <li>
+                    <i class="fas fa-fw fa-link"></i>
+                    <span>${(data.blog) ? data.blog : '-'}</span>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <i class="fab fa-fw fa-twitter"></i>
+                    <span>${(data.twitter_username) ? data.twitter_username : '-'}</span>
+                </li>
+                <li>
+                    <i class="fas fa-fw fa-building"></i>
+                    <span>${(data.company) ? data.company : '-'}</span>
+                </li>
+            </ul>
+        </div>
+    </div>`
+
+    userCardContainer.innerHTML = userCardHTML
     }
 }
 
